75b129ff49ecd1ef6f58036cd9c378bc8dc56354
Arcadia <arcadia@aias.io>
2019-11-07 20:03:59 +0900

    Fixed to alias rule

--- arca.zsh
+++ arca.zsh
@@ -754,11 +754,21 @@ prompt_arca_setup() {
 }
 
 prompt_arca_setup "$@"
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
 
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias grep='grep --color=auto'
-alias l='ls -CF'
-alias la='ls -A'
-alias ll='ls -alF'
-alias ls='ls -G'
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+else
+	alias egrep='egrep --color=auto'
+	alias fgrep='fgrep --color=auto'
+	alias grep='grep --color=auto'
+	alias l='ls -CF'
+	alias la='ls -A'
+	alias ll='ls -alF'
+	alias ls='ls -G'
+fi
